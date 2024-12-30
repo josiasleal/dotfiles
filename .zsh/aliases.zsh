045ae6197b160bfc5763bd1ac8ade0e3a6c40272
Josias Leal <josias.leal@gmail.com>
2024-12-30 21:05:51 +0000

    teste

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,23 +1,19 @@
 # Aliases
 # ---
 #
-alias k="kubectl"
-alias h="helm"
-alias tf="terraform"
-alias tg="terragrunt"
-alias a="ansible"
-alias ap="ansible-playbook"
-alias top="btop"
-alias htop="btop"
+alias tg='terragrunt'
+alias a='ansible'
+alias ap='ansible-playbook'
+alias top='btop'
+alias htop='btop'
 
-alias code="open -a 'Visual Studio Code'"
+alias dl='docker ps -l -q'
+# alias gprj='cd $HOME/Projects'
+# alias gcs='cd $HOME/Projects/christianlempa/cheat-sheets'
+# alias gbp='cd $HOME/Projects/christianlempa/boilerplates'
 
-# alias gprj="cd $HOME/Projects"
-# alias gcs="cd $HOME/Projects/christianlempa/cheat-sheets"
-# alias gbp="cd $HOME/Projects/christianlempa/boilerplates"
-
-# alias ls="eza --icons --group-directories-first"
-# alias ll="eza --icons --group-directories-first -l"
+# alias ls='eza --icons --group-directories-first'
+# alias ll='eza --icons --group-directories-first -l'
 #alias grep='grep --color'
 
-#alias prx="ssh root@prx-prod-"
+#alias prx='ssh root@prx-prod-'
